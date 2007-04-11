fbc6f9d25b7c6a5e26f44497521c80ebf8703cee
Masanori ITOH <masanori.itoh@gmail.com>
2007-04-11 12:57:36 +0000

    rye 2.0 * Added CPU consumption monitoring. * Fixed typos and gramatical errors and added description of the new feature   in README.txt

--- README.txt
+++ README.txt
@@ -1,40 +1,67 @@
 NAME
   rye.ko - a catcher of in-kernel events
 
-SYSNOPSYS
-  insmod rye.ko ptr=<pointer to a in-kernel function> [OPTIONs]
+SYNOPSIS
+  insmod rye.ko [OPTIONs]
 
 DESCRIPTION
 
   This small module, rye.ko, tries to catch a specific in-kernel event.
-
-  At this moment the only supported event is a phenomenon called CSS
-  (Context Switch Storm) [1, 2, 3].
+  At this moment, supported events are a phenomenon called CSS
+  (Context Switch Storm) [1, 2, 3] and CPU usage ratio.
   rye.ko creates a kernel thread on insertion, and the thread monitors
-  accumulated number of context switches periodically and tests if context
-  switches rate (per second) gets higher than a threshold given on the
-  insertion time periodically.
-  If the rate exceeded the given threshold, it calls panic(). Thus,
-  using rye.ko with your favorite crash dump utility such as kdump,
-  you can take good information for your investigation.
+  periodically a specified quantity such as context switches rate.
+  If the quantity under monitoring exceeded the given threshold,
+  it calls panic(). Thus, using rye.ko with your favorite crash dump
+  utility such as kdump, you can take good information for your investigation.
 
-  Here are description of insmod perameters of rye.ko.
-    ptr           A pointer to nr_context_switch
-                  You have to provide 'ptr', otherwise insmod fails.
-    interval	  Interval in seconds to monitor number of context switches
-    count         Monitoring count
-    threshold     Threshold value to take a CSS event
-                  If cswch/s is higher than the threshold, rye calls panic().
-    action         Action in case caught a CSS event
+  Here are description of insmod parameters.
+    event         Event type to catch
+                  0 : Monitors if number of context switches per second exceeds
+                      the given threshold. (default)
+                  1 : Monitors if specified categories of CPU consumption rate
+                      exceeds the given threshold.
+    interval	  Interval in seconds to monitor the specified quantity.
+                  Default is 60 seconds.
+    count         Monitoring count. Default is 2.
+    threshold     Threshold value to recognize event occurrences.
+                  In case event=0, it means context switches per second.
+                  In case event=1, it means CPU consumption rate. (0-100)
+                  Default is 30000.
+    action        Action in case rye.ko caught the specified event
+                  0 : just shows a message via printk() (default)
+                  1 : causes a panic via panic()
+    ptr           A pointer to nr_context_switches().
+                  When event is 0, you have to provide 'ptr', otherwise
+                  insmod fails.
+    cpu_mask      Bit field for CPU consumption categories to monitor.
+                  0x8000 : %user
+                  0x4000 : %nice
+                  0x2000 : %system
+                  0x1000 : %softirq
+                  0x0800 : %irq
+                  0x0400 : %idle (default)
+                  0x0200 : %iowait
+                  0x0100 : %steal (kernel version 2.6.12 or later)
     verbose       Verbose mode
+                  0 : quiet output (default)
+                  non zero : verbose output
 
-  Note that the in-kernel function 'nr_context_switches' is not a publicly
-  exported function to general loadable kernel modules(lkm), so you have to get
-  the address from your running kernel via /proc/kallsyms.
+  Here are several remarks.
 
-  Currently, tested platforms are x86 and x86_64. I'm not sure other
-  platforms cause I don't have other architecture boxes, but I believe
-  basically it should work.
+  * The in-kernel function 'nr_context_switches()' is not a publicly
+    exported to general loadable kernel modules, so you have to get
+    the address from your running kernel through /proc/kallsyms.
+
+  * CPU consumption rates are calculated as the same way with utilities 
+    contained in 'sysstat' package such as 'sar'.
+    That is, increased CPU time in a particular category such as user mode
+    will be divided by total CPU time between two sampling timings, not
+    by the actual interval time between the two timings.
+
+  * Currently, tested platforms are x86 and x86_64. I'm not sure other
+    platforms cause I don't have other architecture boxes, but I believe
+    basically it should work.
 
 INSTALLATION AND USAGE
 
@@ -55,8 +82,8 @@ INSTALLATION AND USAGE
 
    In case of my PentiumD 930 FedoraCore6 box, PATH_TO_KERNEL_SOURCE is:
       /usr/src/kernels/2.6.18-1.2798.fc6-xen-x86_64
-   You yould see 'rye.ko' in your current directory.
 
+   After compilation, you should see 'rye.ko' in your current directory.
 
   5) Install it (if you like)
 
@@ -65,42 +92,51 @@ INSTALLATION AND USAGE
 
       # make -C PATH_TO_KERNEL_SOURCE SUBDIRS=`pwd` V=1 modules_install
 
-
-  6) Get nr_context_switches address and choose parameters
+  6) Get nr_context_switches address and choose parameters, if needed
 
     Below is my FC6(x86_64) box case:
 
       # grep nr_context_switches /proc/kallsyms
       ffffffff80280d78 T nr_context_switches
 
-    Here, for example, let's use threshold=30000 (30000 cswch/s), 
-    interval=60 (60 secs, this is default), count=60 (in total, monitors
-    one hour) and action=1 (take a crash dump).
 
   7) Insert it into your running kernel
 
+    Here, for example, let's use threshold=30000 (30000 cswch/s), 
+    interval=60 (60 secs, this is default), count=60 (in total, monitors
+    one hour) and action=1 (cause a panic and take a crash dump).
     Then, insmod command line would be like the following:
 
       # insmod rye.ko ptr=0xffffffff80280d78 threshold=30000 count=60 action=1
 
-    If you omit action=1, rye doesn't call panic() and just show cswch/s value
-    exeeded the threshold. Do not forget to add a prefix '0x' to the ptr value.
+    If you want to catch an event in which system (kernel) consumes more than
+    80% (normalized to 100%) of CPU power, do like the following:
 
-  8) Cause a CSS by any method whatever you like
+      # insmod rye.ko event=1 threshold=80 count=60 action=1 cpu_mask=0x2000
 
+    Note that the above example 'cpu_mask0x2000' does not include softirq nor
+    irq. If you want to monitor CPU time in kernel mode, use 'cpu_mask=0x3800'
+    instead.
 
-NOTE
-  Originally, this module was called 'csspanic', but this module can be
-  a good start point for monitoring various in-kernel quantities such as
-  CPU busy rate. So,it's changed.
+    If you omit action=1 in both cases, rye.ko doesn't call panic() and
+    just show the quantity exceeded the threshold.
+    Also, do not forget to add a prefix '0x' to values of 'ptr' value.
 
-  Someone might feel that there might some connections with a novel
-  by J.D. Salinger[4] or deeper thing[5], but never mind that. :)
+  8) Cause the event by any method whatever you like
+
 
 LICENSE
   This module is provided under GPL2 as you can see in rye.c.
 
   Copyright (C) 2007 Masanori ITOH <masanori.itoh@gmail.com>
+
+HISTORY
+  Originally, this module was called 'csspanic', but this module can be
+  a good start point for monitoring various in-kernel quantities such as
+  CPU busy rate. So,it's changed.
+
+  Someone might feel that there could be some connections with a novel
+  by J.D. Salinger[4] or deeper thing[5], but never mind that. :)
 
 REFERENCES
   [1] http://archives.postgresql.org/pgsql-performance/2006-11/msg00037.php
--- rye.c
+++ rye.c
@@ -22,6 +22,7 @@
  *
  * $Id$
  */
+#include <linux/version.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -30,9 +31,20 @@
 #include <linux/types.h>
 
 MODULE_AUTHOR("Masanori ITOH");
-MODULE_DESCRIPTION("Catches a CSS and causes a panic.");
+MODULE_DESCRIPTION("Catches a specified in-kernel event and causes a panic.");
 MODULE_LICENSE("GPL");
-MODULE_VERSION("1.0");
+MODULE_VERSION("2.0");
+
+#define RYE_CSW 0
+#define RYE_CPU 1
+#define RYE_CPU_USER    0x8000
+#define RYE_CPU_NICE    0x4000
+#define RYE_CPU_SYSTEM  0x2000
+#define RYE_CPU_SOFTIRQ 0x1000
+#define RYE_CPU_IRQ     0x0800
+#define RYE_CPU_IDLE    0x0400
+#define RYE_CPU_IOWAIT  0x0200
+#define RYE_CPU_STEAL   0x0100
 
 /*
  * Ugly workaround.
@@ -63,23 +75,196 @@ static int action = 0;
 module_param(action, int, 0644);
 MODULE_PARM_DESC(action, "action, set non-zero to cause a panic");
 
+static int event = 0;
+module_param(event, int, 0644);
+MODULE_PARM_DESC(event, "event to monitor, 0:csw, 1:cpu");
+
+static int cpu_mask = RYE_CPU_IDLE;
+module_param(cpu_mask, int, 0644);
+MODULE_PARM_DESC(cpu_mask, "user/nice/system/softirq/irq/idle/steal...");
+
+
+struct rye_sample {
+    union {
+	struct {
+	    u64 user;
+	    u64 nice;
+	    u64 system;
+	    u64 softirq;
+	    u64 irq;
+	    u64 idle;
+	    u64 iowait;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,12)
+	    u64 steal;
+#endif
+	    u64 total;
+	} cpu;
+	unsigned long long csw;
+    } s;
+    int num_cpu;
+};
+
 /*
- * Some global variables
+ * In case of 'nr_context_switches()' defined in kernel/sched.c,
+ * it refers per cpu structures, runqueue.
  */
-static unsigned long long max_csw;
-unsigned long long (*func)(void);
+unsigned long long (*csw_func)(void);
+
+static void (*rye_sample)(struct rye_sample *) ;
+static int (*rye_check)(struct rye_sample *, struct rye_sample *, long, long);
+
+/*
+ * event == 0 (RYE_CSW)
+ */
+static int rye_check_csw(struct rye_sample *now,
+			 struct rye_sample *before,
+			 long sec, long usec)
+{
+    unsigned long divisor;
+    unsigned long long csw_sec;
+    
+    csw_sec = (now->s.csw - before->s.csw) * (1000 * 1000);
+    divisor = (unsigned long)(1000 * 1000 * sec + usec);
+    do_div(csw_sec, divisor);
+    if(csw_sec >= threshold) {
+	printk("rye_check_csw: "
+	       "%lld (csws/sec), %llu (csws) while %ld.%06ld (sec)\n",
+	       csw_sec,
+	       (now->s.csw - before->s.csw),
+	       sec,
+	       usec);
+	return 1;
+    } else if (verbose) {
+	printk("rye_check_csw: %llu (cswch/s)\n", csw_sec);
+    }
+    
+    return 0;
+}
+
+
+static void rye_sample_csw(struct rye_sample *p) 
+{
+    p->s.csw = (*csw_func)();
+    return;
+}
+
+
+/*
+ * event == 1 (RYE_CPU)
+ */
+static int rye_check_cpu(struct rye_sample *now,
+			 struct rye_sample *before,
+			 long sec, long usec)
+{
+    u64 diff_total, work = 0;
+
+    diff_total = now->s.cpu.total - before->s.cpu.total;
+
+    if (cpu_mask & RYE_CPU_USER) {
+	work += (now->s.cpu.user - before->s.cpu.user);
+    }
+    if (cpu_mask & RYE_CPU_NICE) {
+	work += (now->s.cpu.nice - before->s.cpu.nice);
+    }
+    if (cpu_mask & RYE_CPU_SYSTEM) {
+	work += (now->s.cpu.system - before->s.cpu.system);
+    }
+    if (cpu_mask & RYE_CPU_SOFTIRQ) {
+	work += (now->s.cpu.softirq - before->s.cpu.softirq);
+    }
+    if (cpu_mask & RYE_CPU_IRQ) {
+	work += (now->s.cpu.irq - before->s.cpu.irq);
+    }
+    if (cpu_mask & RYE_CPU_IDLE) {
+	work += (now->s.cpu.idle - before->s.cpu.idle);
+    }
+    if (cpu_mask & RYE_CPU_IOWAIT) {
+	work += (now->s.cpu.iowait - before->s.cpu.iowait);
+    }
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,12)    
+    if (cpu_mask & RYE_CPU_STEAL) {
+	work += (now->s.cpu.steal - before->s.cpu.steal);
+    }
+#endif
+    work *= 100;
+#ifdef DEBUG_CPU    
+    printk("rye_check_cpu: DEBUG: %llu, %llu\n", diff_total, work);
+#endif
+    do_div(work, (uint32_t)diff_total);
+    do_div(work, (uint32_t)now->num_cpu);
+
+    if (work >= (unsigned long long)threshold) {
+	printk("rye_check_cpu: %llu (%%)\n", work);	
+	return 1;
+	
+    } else if (verbose) {
+	printk("rye_check_cpu: %llu (%%)\n", work);
+    }
+    return 0;
+}
+
+static void rye_sample_cpu(struct rye_sample *p)
+{
+    int cpu;
+
+    memset(p, 0x0, sizeof(struct rye_sample));
+
+    for_each_cpu(cpu) {
+	p->s.cpu.user += kstat_cpu(cpu).cpustat.user;
+	p->s.cpu.nice += kstat_cpu(cpu).cpustat.nice;
+	p->s.cpu.system += kstat_cpu(cpu).cpustat.system;
+	p->s.cpu.softirq += kstat_cpu(cpu).cpustat.softirq;
+	p->s.cpu.irq += kstat_cpu(cpu).cpustat.irq;
+	p->s.cpu.idle += kstat_cpu(cpu).cpustat.idle;
+	p->s.cpu.iowait += kstat_cpu(cpu).cpustat.iowait;
+	p->num_cpu++;
+    }
+    p->s.cpu.total =
+	p->s.cpu.user +
+	p->s.cpu.nice +
+	p->s.cpu.system +
+	p->s.cpu.softirq +
+	p->s.cpu.irq +
+	p->s.cpu.idle +
+	p->s.cpu.iowait
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,12)
+	+
+	p->s.cpu.steal
+#endif
+	;
+#ifdef DEBUG_CPU
+    printk("rye_sample_cpu: DEBUG: %d, %llu %llu %llu %llu %llu %llu %llu "
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,12)
+	   "%llu "
+#endif	   
+	   "\n", 
+	   p->num_cpu,
+	   p->s.cpu.user,
+	   p->s.cpu.nice,
+	   p->s.cpu.system,
+	   p->s.cpu.softirq,
+	   p->s.cpu.irq,
+	   p->s.cpu.idle,
+	   p->s.cpu.iowait
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,12)
+	   ,
+	   p->s.cpu.steal
+#endif
+	   );
+#endif
+    return;
+}
 
 static int rye_thread(void *arg)
 {
     int ret, c;
     struct timeval now, before;
     long sec, usec;
-    unsigned long long csw_before, csw_now, csw_sec;
-    unsigned long divisor;
+    struct rye_sample rs_before, rs_now;
     
     printk("rye_thread: created\n");
-
-    csw_before = (*func)();
+    
+    (*rye_sample)(&rs_before);
     do_gettimeofday(&before);
     
     for (c = 1; c <= count; c++) {
@@ -90,8 +275,8 @@ static int rye_thread(void *arg)
 	    printk("rye_thread: schedule_timeout returns. %d\n", ret);
 	    return 1;
 	}
-
-	csw_now = (*func)();
+	
+	(*rye_sample)(&rs_now);
 	do_gettimeofday(&now);
 	if (now.tv_usec < before.tv_usec) {
 	    /*
@@ -103,35 +288,17 @@ static int rye_thread(void *arg)
 	    sec = now.tv_sec - before.tv_sec;
 	    usec = now.tv_usec - before.tv_usec;
 	}
-	
-	csw_sec = (csw_now - csw_before) * (1000 * 1000);
-	divisor = (unsigned long)(1000 * 1000 * sec + usec);
-	do_div(csw_sec, divisor);
-	if (verbose) {
-	    printk("rye_thread: %llu (cswch/s)\n", csw_sec);
-	}
-	
-	if(csw_sec >= max_csw) {
+	ret = (*rye_check)(&rs_now, &rs_before, sec, usec);
+	if(ret) {
 	    if (action) {
-		panic("Got you! :)  "
-		      "%lld (csws/sec), %llu (csws) while %ld.%06ld (sec)\n",
-		      csw_sec,
-		      (csw_now - csw_before),
-		      sec,
-		      usec);
+		panic("rye_thread: Got you! :)\n");
+			/* never here*/
 	    } else {
-		printk("Got you! :)  "
-		      "%lld (csws/sec), %llu (csws) while %ld.%06ld (sec)\n",
-		      csw_sec,
-		      (csw_now - csw_before),
-		      sec,
-		      usec);
-		
+		printk("rye_thread: Got you! :)\n");
 	    }
-	    /* never here*/
 	}
+	rs_before.s.csw = rs_now.s.csw;
 	
-	csw_before = csw_now;
 	before = now;
     }
     printk("rye_thread: return.\n");
@@ -141,22 +308,45 @@ static int rye_thread(void *arg)
 static int __init rye_init(void)
 {
     int ret;
-    
-    printk("rye_init: interval=%d, count=%d threshold=%ld ptr=%p\n",
-	   interval, count, threshold, (void *)ptr);
-    printk("rye_init: verbose=%d, action=%d\n",
-	   verbose, action);
-    
-    max_csw = threshold;
 
-    func = (unsigned long long (*)(void))ptr;
-    if (func == 0) {
-	printk("rye_init: specify nr_context_switch address.\n");
-	return -EINVAL;
+    printk("rye_init: event=%d, interval=%d, count=%d\n",
+	   event, interval, count);
+    printk("rye_init: threshold=%ld, action=%d, verbose=%d\n",
+	   threshold, action, verbose);
+    printk("rye_init: ptr=0x%p, cpu_mask=0x%0x\n",
+	   (void *)ptr, cpu_mask);
+    
+    switch (event) {
+    case RYE_CSW:
+	rye_sample = rye_sample_csw;
+	rye_check = rye_check_csw;
+	csw_func = (unsigned long long (*)(void))ptr;
+	if (csw_func == 0) {
+	    printk("rye_init: specify nr_context_switch address.\n");
+	    return -EINVAL;
+	}
+	break;
+
+    case RYE_CPU:
+	if ((threshold < 0) || (threshold > 100)) {
+	    printk("rye_init: invalid threshold for event=1: %ld.\n",
+		   threshold);
+	    return -EINVAL;
+	}
+	rye_sample = rye_sample_cpu;
+	rye_check = rye_check_cpu;
+	break;
+
+    default:
+	printk("rye_init: invalid event: %d.\n", event);
+	return -EINVAL;	
     }
-
-    ret = kernel_thread(rye_thread, NULL, CLONE_KERNEL);
     
+    ret = kernel_thread(rye_thread, NULL, CLONE_KERNEL);
+    if (ret < 0) {
+	printk("rye_init: Failed to create a kernel thread.\n");
+	return -ENOMEM;
+    }
     return 0;
 }
 
