cb579b8ad1c4e75aa03742366a269eb6d1d76c04
Masanori ITOH <masanori.itoh@gmail.com>
2008-04-22 23:45:00 +0000

    1. Initial revision of pfr (a periodical file content recorder).

---
+++ pfr.c
@@ -0,0 +1,144 @@
+/*
+ * pfr.c : a simple periodical file content recorder:
+ *
+ * Copyright (C) 2008  Masanori ITOH <masanori.itoh_at_gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * $Id$
+ */
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFSIZE 4096
+
+int main(int argc, char **argv)
+{
+	int fd, ret, count = 0, max_count = 1, bufsize = BUFSIZE;
+	int c, interval = 1, debug = 0;
+	int out = 1; /* STDOUT */ 
+	char *pbuf, buf[BUFSIZE], bufo[BUFSIZE];
+	char *cr = "\n";
+	char *file = NULL, *ofile = NULL;
+	time_t now;
+	struct tm *tm;
+
+	pbuf = buf;
+	while ((c = getopt(argc, argv, "f:c:i:o:b:d")) != -1) {
+		switch (c) {
+		case 'f':
+			file = optarg;
+			break;
+
+		case 'c':
+			max_count = atoi(optarg);
+			break;
+
+		case 'b':
+			bufsize = atoi(optarg);
+			pbuf = malloc(bufsize);
+			break;
+
+		case 'i':
+			interval = atoi(optarg);
+			break;
+
+		case 'o':
+			ofile = optarg;
+			break;
+
+		case 'd':
+			debug = 1;
+			break;
+		}
+	}
+	if (debug) {
+		printf("#file     = '%s'\n", file);
+		printf("#ofile    = '%s'\n",
+		       (ofile == NULL) ? "STDOUT" : ofile);
+		printf("#count    = %d\n", max_count);
+		printf("#interval = %d\n", interval);
+		printf("#bufsize  = %d\n", bufsize);
+	}
+	
+	if (ofile != NULL) {
+		out = open(optarg, O_RDWR | O_CREAT | O_TRUNC, 00666);
+		if (out < 0) {
+			printf("Cannot open '%s'\n", optarg);
+			exit(1);
+		}
+	}
+    
+	if (file == NULL) {
+		printf("Specify a file to monitor.\n");
+		exit(1);
+		
+	} else {
+		fd = open(file, O_RDONLY);
+		if (fd < 0) {
+			printf("failed to open '%s'\n", file);
+			exit(1);
+		}
+	}
+
+	sprintf(bufo, "# %s\n", file);
+	write(out, bufo, strlen(bufo));
+    
+	count = 0;
+	while (1) {
+		lseek(fd, SEEK_SET, 0);
+		memset(pbuf, 0x0, bufsize);
+
+		now = time(NULL);
+		ret = read(fd, pbuf, bufsize);
+		if (debug) {
+			printf("#read returns = %d\n", ret);
+		}
+		if (ret < 0) {
+			printf("read %s failed.\n", file);
+			exit(1);
+		}
+
+		tm = localtime(&now);
+		sprintf(bufo, "%04d/%02d/%02d %02d:%02d:%02d\n",
+			tm->tm_year + 1900, tm->tm_mon, tm->tm_mday,
+			tm->tm_hour, tm->tm_min, tm->tm_sec);
+		write(out, bufo, strlen(bufo));
+		write(out, cr, strlen(cr));
+		write(out, pbuf, ret);
+		if (ret == bufsize) {
+			printf("\n# Warning! Need more than 2 read()."
+			       " Consider increasing buffer size (-b)\n");
+			while ((ret = ret = read(fd, pbuf, bufsize)) > 0) {
+				write(out, pbuf, ret);
+			}
+		}
+		write(out, cr, strlen(cr));
+
+		count++;
+		if (count >= max_count) break;
+		sleep(interval);
+	}
+	close(fd);
+
+	return 0;
+}
+
